5295dd82c6ca696e91c7ec74f5eff5fd73271f76
Keith Bender <>
2020-02-13 19:22:26 -0600

    finished server

--- server/server.js
+++ server/server.js
@@ -144,6 +144,43 @@ app.post('/api/product/article', auth, admin, (req, res) => {
   })
 })
 
+app.get('/api/product/articles_by_id', (req, res) => {
+  let type = req.query.type;
+  let items = req.query.id;
+
+  if(type === "array"){
+    let ids = req.query.id.split(',');
+    items = [];
+    items = ids.map((item) => {
+      return mongoose.Types.ObjectId(item);
+    })
+  }
+
+  Product.find({'_id': {$in:items}})
+  .populate('brand')
+  .populate('wood')
+  .exec((err, docs) => {
+    return res.status(200).send(docs);
+  })
+})
+
+//By Arrival
+app.get('/api/products/articles', (req, res) => {
+  let order = req.query.order ? req.query.order : 'asc';
+  let sortBy = req.query.sortBy ? req.query.sortBy : '_id';
+  let limit = req.query.limit ? parseInt(req.query.limit) : 100;
+
+  Product.find().
+  populate('brand').
+  populate('wood').
+  sort([[sortBy, order]]).
+  limit(limit).
+  exec((err, articles) => {
+    if(err) return res.status(400).send(err);
+    res.send(articles);
+  })
+})
+
 const port = process.env.PORT || 5001;
 
 app.listen(port, () => {
